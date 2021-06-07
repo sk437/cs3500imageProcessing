dbe95994ee3f6d76605743004b5a8f634a8be0b6
mkgithub-acc <krstulovic.m@northeastern.edu>
2021-06-07 12:57:35 -0400

    Small comment update

--- imageProcessingProject/src/filters/Filter.java
+++ imageProcessingProject/src/filters/Filter.java
@@ -1,7 +1,7 @@
 package filters;
 
 /**
- * Represents a filter which can be applied to a graph of pixels.
+ * Represents a filter which can be applied to a graph of pixels to alter an image.
  */
 public interface Filter {
 }
